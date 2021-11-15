7a9bc2f5c88df34ce9ea8ea9ce8ec2a9ce02f14c
jinyao948 <jinyao948@gmail.com>
2021-11-15 21:36:20 +0800

    base routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-function App() {
-  return (
-    <div >
-     Own It!
-    </div>
-  );
-}
+import { BrowserRouter as  Routes, Route} from "react-router-dom";
+import { Spin } from "antd";
+import React, { useState} from "react";
+import Landing from "./components/landing/Landing";
+import Navigation from "./components/nav/Navigation";
 
-export default App;
+export default function App() {
+  const [loading, setLoading] = useState(true)
+  
+   return (
+        <>
+            <Navigation />
+            {loading ? (
+                <div className="spin-large">
+                    <Spin size="large" />
+                </div>
+            ) : ( 
+                <Routes>
+                    <Route exact path="/" component={Landing} />
+                </Routes>
+            )}
+        </>
+    );
+}
