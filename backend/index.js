2764508517e626493c771b4c022b47bc1ab32837
jinyao948 <jinyao948@gmail.com>
2021-11-15 17:58:58 +0800

    reorg folders

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+// onst express = require("express");
+
+// const PORT = process.env.PORT || 3000;
+
+// const app = express();
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+///we can use nodemon
