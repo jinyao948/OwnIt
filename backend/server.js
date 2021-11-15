acd893f0cfa3db3655f9546f81e806fdce35998e
jinyao948 <jinyao948@gmail.com>
2021-11-15 18:07:34 +0800

    backend setup

---
+++ backend/server.js
@@ -0,0 +1,6 @@
+// require("dotenv").config();
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const cookieParser = require("cookie-parser");
+// const cors = require('cors');
+// const authRoutes = require("./routes/authRoutes");
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
   return (
     <div >
-      Hello World
+     Own It!
     </div>
   );
 }
