3c8f09ca5a26c7ea505602e814e0ab31504158b1
WenHan <wenhantan94@gmail.com>
2021-11-17 20:42:31 +0800

    frontend skeleton done

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,9 +1,28 @@
+import "./Landing.css";
+import { Input, Button } from "antd";
 
-export default function Landing() {
-  return (
-    <h1 classname="landing">
-      This is the landing page
-    </h1>
-  );
+export default function Landing() { 
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(event.target.email.value);
+    };
+
+    return (
+        <div className="landing">
+            <h1 className="title">OwnIt!</h1>
+            <h2 className="subtitle">
+                Don't find your next customer, let them find you!
+            </h2>
+            <h3 className="mailing-list">Join our mailing list to find out more!</h3>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    name="email"
+					placeholder="Email"
+                />
+                <input type="submit" value="Submit" className="submit-button" />
+            </form>
+        </div>
+    );
 }
-
